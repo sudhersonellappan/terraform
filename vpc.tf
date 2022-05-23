379b00bcac87b6c5ce15968a3500c73999793c01
sudhersonellappan <105734001+sudhersonellappan@users.noreply.github.com>
2022-05-23 23:54:18 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,11 @@
+resource “aws_vpc” “prod-vpc” {
+    cidr_block = “10.0.0.0/16”
+    enable_dns_support = “true” #gives you an internal domain name
+    enable_dns_hostnames = “true” #gives you an internal host name
+    enable_classiclink = “false”
+    instance_tenancy = “default”    
+    
+    tags {
+        Name = “prod-vpc”
+    }
+}
